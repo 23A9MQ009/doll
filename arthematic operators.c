67b630e14983a9308d2c9aabaf515db6235c4198
23A9MQ009 <147475509+23A9MQ009@users.noreply.github.com>
2023-10-11 21:17:18 +0530

    Add files via upload

---
+++ arthematic operators.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+main()
+{
+	int a,b;
+	printf("\n enter 2 integers");
+	scanf("%d%d",&a,&b);
+	printf("\n value of sum is %d",a+b);
+	printf("\n value of difference if %d",a-b);
+}
---
+++ ascii.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+
+  if ('a'>'A')
+  {
+	printf("YES");
+  }
+  else
+  {
+  	printf("NO");
+  }
+}
